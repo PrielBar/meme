df0ff76b4f62ae6eaf7a49a5e0c0f969874a1268
PrielBar <87776799+PrielBar@users.noreply.github.com>
2021-08-11 20:09:40 +0300

    uploud

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function drawImgFromlocal() {
+    var img = new Image()
+    img.src = 'img/1.jpg';
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
+    }
+}
